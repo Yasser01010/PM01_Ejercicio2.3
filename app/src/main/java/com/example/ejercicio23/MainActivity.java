0131f3ce76c3391ccc2bcfe436e2401fa5229607
Yasser01010 <107781351+Yasser01010@users.noreply.github.com>
2022-07-15 22:12:12 -0600

    Ejercicio 2.3 - Programacion Movil I

---
+++ app/src/main/java/com/example/ejercicio23/MainActivity.java
@@ -0,0 +1,194 @@
+package com.example.ejercicio23;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.ejercicio23.configuraciones.SQLiteConexion;
+import com.example.ejercicio23.configuraciones.Transacciones;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    EditText txtDescripcion;
+    Button btnSalvar,btnVer;
+    ImageView imageView;
+    static final int PETICION_CAM = 100;
+    static final int TAKE_PIC_REQUEST = 101;
+    Bitmap imgUser;
+    SQLiteConexion conexion;
+    Bitmap image;
+    boolean validarfoto;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        conexion = new SQLiteConexion(this, Transacciones.NameDatabase,null,1);
+
+        validarfoto=false;
+
+        txtDescripcion = (EditText) findViewById(R.id.textDescripcion);
+        btnSalvar = (Button) findViewById(R.id.btnSalvar);
+        btnVer = (Button) findViewById(R.id.btnVerPhotograh);
+        imageView = (ImageView) findViewById(R.id.imageViewPhotograh);
+
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                permisos();
+                return false;
+            }
+        });
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!validarfoto){
+                    Toast.makeText(getApplicationContext(), "Ingrese una imagen"
+                            ,Toast.LENGTH_LONG).show();
+                }else if(txtDescripcion.getText().toString().trim().isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Ingrese una descripcion"
+                            ,Toast.LENGTH_LONG).show();
+                }else{
+                    savePhotograh();
+                    txtDescripcion.setText("");
+                    imageView.setImageResource(R.drawable.userr);
+                    validarfoto=false;
+                }
+
+            }
+        });
+
+        btnVer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), viewPhotograh.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+
+    private void savePhotograh() {
+
+        SQLiteDatabase db = conexion.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put(Transacciones.descripcion, txtDescripcion.getText().toString());
+
+        String img= encodeImage(imgUser);
+
+        values.put(Transacciones.imagen, img);
+
+        Long result = db.insert(Transacciones.tablaPhotograh, Transacciones.id, values);
+
+        Toast.makeText(getApplicationContext(), "Photograh guardada correctamente"
+                ,Toast.LENGTH_LONG).show();
+
+        db.close();
+    }
+
+    private void permisos(){
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA)!=
+                PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA},PETICION_CAM);
+        }else{
+            dispatchTakePictureIntent();
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == PETICION_CAM){
+            if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                dispatchTakePictureIntent();
+            }
+        }else{
+            Toast.makeText(getApplicationContext(), "Se necesitan permisos a la camara", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(takePictureIntent.resolveActivity(getPackageManager())!=null){
+            startActivityForResult(takePictureIntent, TAKE_PIC_REQUEST);
+        }
+
+    }
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == TAKE_PIC_REQUEST && resultCode == RESULT_OK){
+
+            Bundle extras = data.getExtras();
+
+             image = (Bitmap) extras.get("data");
+
+            imgUser = image;
+            imageView.setImageBitmap(image);
+            validarfoto=true;
+
+        }
+    }
+
+    private String encodeImage(Bitmap bitmap){
+
+        int previewWidth = 150;
+        int previewHeight = bitmap.getHeight() * previewWidth / bitmap.getWidth();
+
+        Bitmap previewBitmap = Bitmap.createScaledBitmap(bitmap, previewWidth, previewHeight, false);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        previewBitmap.compress(Bitmap.CompressFormat.JPEG, 60, byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+
+        return Base64.encodeToString(bytes, Base64.DEFAULT);
+
+    }
+
+
+
+}
